38f023d86f593d6a4bcd711797d2d5aeb08a0049
Michael <michaelpetterson06.com.br>
2022-09-25 21:03:43 -0300

    .env

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,5 +1,6 @@
+require('dotenv').config();
 const app = require('./api/app');
 
-const PORT = 3000;
+const PORT = process.env.API_PORT || 8080;
 
 app.listen(PORT, () => console.log(`conectado na porta ${PORT}`));
