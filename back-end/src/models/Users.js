92b94c82070d80bcb2b29a6d002c4f31413a03ef
Michael <michaelpetterson06.com.br>
2022-09-27 09:35:45 -0300

    autenticação do token

--- back-end/src/models/Users.js
+++ back-end/src/models/Users.js
@@ -1,18 +1,25 @@
 const connection = require('./connection');
 
+const collection = 'users';
 
 const getAll = async () => connection()
-    .then((db) => db.collection('users').find().toArray());
+    .then((db) => db.collection(collection).find().toArray());
 
 
 const getByEmailPassword = async ({email, password}) => {
     const db = await connection();
-    const user = await db.collection('users').findOne({email, password});
+    const user = await db.collection(collection).findOne({email, password});
     return user;
 }
 
+const getEmail = async (email) => {
+    const db = await connection();
+    const user = await db.collection(collection).findOne({email});
+    return user;
+}
 
 module.exports = {
     getAll,
-    getByEmailPassword
+    getByEmailPassword,
+    getEmail
  };
--- back-end/src/routers/Users.js
+++ back-end/src/routers/Users.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const Users = require('../controllers/Users');
 
+const token = require('../middlewares/token/authToken');
+
 const router = express.Router();
 
+
+router.post('/',token);
 router.get('/', Users.getAll);
 
 
